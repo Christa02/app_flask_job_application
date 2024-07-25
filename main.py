089a49bc740d4b052ddcff7bbb458a3821913f50
CSL <christasaraluke19@gmail.com>
2024-07-25 11:32:57 -0700

    Created DB, Store user data in DB AND showing notification after submit.

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "my_application123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
 
 
-@app.route('/')
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        start_date = request.form['start_date']
+        date_obj = datetime.strptime(start_date, "%Y-%m-%d")
+        occupation = request.form['occupation']
+
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, occupation=occupation)
+        db.session.add(form)
+        db .session.commit()
+        flash("Your form was submitted successfully!")
+
     return render_template("index.html")
 
 
-app.run(debug=True, port=500)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=500)
